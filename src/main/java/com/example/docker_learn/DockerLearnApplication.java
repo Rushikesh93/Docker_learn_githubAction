7bc3cc4fe2a2ae4af2ecce6db132094f31fe46cb
Rushikesh Patil <rushikeshpatil9393@gmail.com>
2026-05-09 20:30:05 +0530

    Initial commit

---
+++ src/main/java/com/example/docker_learn/DockerLearnApplication.java
@@ -0,0 +1,14 @@
+package com.example.docker_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+DockerLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLearnApplication.class, args);
+	}
+
+}
